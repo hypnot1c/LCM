8bd37c9d3db0ffeea6c6c8e1f9679e0bae64d744
ahmed.apanasov <ahmed.apanasov@winpharma.com>
2017-01-18 15:20:51 +0300

    Add Prism.Unity Add app shell page

---
+++ src/LCM.UWP_App/Helpers/AppPagesHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace LCM.UWP_App.Helpers
+{
+    public class AppPagesHelper
+    {
+        public const string MainPage = "Main";
+    }
+}
---
+++ src/LCM.UWP_App/Pages/ShellPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace LCM.UWP_App.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ShellPage : Page
+    {
+        public ShellPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public void SetContentFrame(Frame frame)
+        {
+            //rootSplitView.Content = frame;
+        }
+
+        public void SetMenuPaneContent(UIElement content)
+        {
+            //rootSplitView.Pane = content;
+        }
+    }
+}
